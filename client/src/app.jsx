036ea7241f639d7e270da5264d2ac42f177712cf
Julia Ramos <juliaramosguedes@gmail.com>
2020-05-18 14:53:30 -0300

    feat(mvp) redirect if wrong url

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,7 +1,12 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import dotEnv from "dotenv";
-import { HomePage } from "./pages";
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+import dotEnv from 'dotenv';
+import { HomePage } from './pages';
 
 function App() {
   dotEnv.config();
@@ -12,6 +17,7 @@ function App() {
         <Route exact path="/">
           <HomePage minutes={1} />
         </Route>
+        <Redirect to="/" />
       </Switch>
     </Router>
   );
