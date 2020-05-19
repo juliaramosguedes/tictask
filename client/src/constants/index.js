78758960ca8314cb254edc3123953ce3006a2920
Julia Ramos <juliaramosguedes@gmail.com>
2020-05-18 21:01:08 -0300

    feat(mvp) fix time

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,18 +1,18 @@
 export const INTERVAL = {
   POMODORO: {
-    TIME: 0.2,
+    TIME: 25,
     NAME: 'Hora do Pomodoro',
     PHRASE: 'Concentre-se!',
     KEY: 'POMODORO',
   },
   SHORTBREAK: {
-    TIME: 0.2,
+    TIME: 5,
     NAME: 'Hora do intervalo',
     PHRASE: 'Descanse um pouco.',
     KEY: 'SHORTBREAK',
   },
   LONGBREAK: {
-    TIME: 0.2,
+    TIME: 15,
     NAME: 'Hora do intervalo longo',
     PHRASE: 'Faça um intervalo maior dessa vez.',
     KEY: 'LONGBREAK',
