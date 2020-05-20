015aab94825215875ddebff96c67558002f340db
Julia Ramos <juliaramosguedes@gmail.com>
2020-05-20 18:24:33 -0300

    task(sw) try waituntil sw

--- client/public/sw.js
+++ client/public/sw.js
@@ -8,17 +8,25 @@ self.addEventListener('activate', (event) => {
 
 let intervalRef;
 
-self.addEventListener('message', ({ data, source }) => {
+self.addEventListener('message', ({ data, source, waitUntil }) => {
   let timeLeft = data.time;
   if (data.stop) {
     clearInterval(intervalRef);
+    intervalRef = null;
     source.postMessage(0);
     return;
+  } else if (!intervalRef) {
+    waitUntil(
+      new Promise((resolve) => {
+        intervalRef = setInterval(() => {
+          if (timeLeft > 0) {
+            timeLeft -= 1;
+            source.postMessage(timeLeft);
+          } else {
+            resolve();
+          }
+        }, 1000);
+      })
+    );
   }
-  intervalRef = setInterval(() => {
-    if (timeLeft > 0) {
-      timeLeft -= 1;
-      source.postMessage(timeLeft);
-    }
-  }, 1000);
 });
